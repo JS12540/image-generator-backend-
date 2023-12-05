1dbe6ba9a5bf8da06b51fe16a5915c098eeff669
lhtjayshah <jayrajesh@leewayhertz.com>
2023-12-05 17:13:05 +0530

    added loggers

--- app.py
+++ app.py
@@ -5,10 +5,18 @@ import torch
 import mediapy as media
 from torch import autocast
 from src.model import process_images
-
+import logging
 
 app = FastAPI()
 
+# Configure logging
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+)
+logger = logging.getLogger(__name__)
+
+# Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -25,14 +33,29 @@ class InputData(BaseModel):
 class OutputData(BaseModel):
     image_data: str
 
+@app.options("/generate_image")
+async def options_generate_image():
+    return {"msg": "Preflight request received"}
+
 @app.post("/generate_image", response_model=OutputData)
 async def generate_image(data: InputData):
-    # Your existing code for image generation
-    prompt = data.prompt
-    num_images = data.num_images
+    try:
+        # Your existing code for image generation
+        prompt = data.prompt
+        num_images = data.num_images
 
-    # Call the modified process_images function
-    image_data = process_images(prompt, num_images)
+        # Log input data
+        logger.info(f"Received request with prompt: {prompt} and num_images: {num_images}")
 
-    return OutputData(image_data=image_data)
+        # Call the modified process_images function
+        image_data = process_images(prompt, num_images)
 
+        # Log success
+        logger.info("Image generation successful")
+
+        return OutputData(image_data=image_data)
+    except Exception as e:
+        # Log the exception
+        logger.error(f"Error during image generation: {str(e)}")
+        # Raise HTTPException with 500 status code and error detail
+        raise HTTPException(status_code=500, detail=str(e))
